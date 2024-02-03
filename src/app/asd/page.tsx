01ad6ad31ea1262b03c1188f8c714951e1841a7c
ArthurKa <arthur.katruk@gmail.com>
2024-02-03 20:02:03 +0200

    asd

---
+++ src/app/asd/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Home() {
+
+  return (
+    <main className="m-auto h-screen max-w-4xl">
+      <Link href='/'>home</Link>
+    </main>
+  );
+}
